2ab61b6cb6b943ca9ab7abf4684dac046e36c29a
Victor Lourenco <victorhlo12@hotmail.com>
2022-06-07 16:42:30 -0300

    inserindo-cursos

---
+++ Aula 06 - Inserindo Cursos.sql
@@ -0,0 +1,57 @@
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(1, 'VBA I', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(2, 'VBA II', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(3, 'VBA III', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(4, 'Excel Básico', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(5, 'Excel Intermediário', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(6, 'Excel Avançado', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(7, 'Pacote Office Completo', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(8, 'Word', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(9, 'PowerPoint', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values
+(10, 'Access', '07/06/2022', 'DESTOP-KS1681N');
+
+insert into Cursos
+(id_curso, nome_curso, data_cadastro, login_cadastro)
+values 
+(11, 'Word Avançado', '07/06/2022', 'DESTOP-KS1681N');
+
+
+select * from dbo.Cursos;
