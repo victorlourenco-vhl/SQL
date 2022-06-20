59d54149fe1d1e82dffb86036ca421cbe15b338b
Victor Lourenco <victorhlo12@hotmail.com>
2022-06-20 11:09:56 -0300

    gerando-backups

---
+++ MySQL/Aula 08 - Gerenciando copias de Seguranca.sql
@@ -0,0 +1,12 @@
+insert into cursos values
+('1', 'HTML5', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Progamação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PHP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5', 'Java', 'Introdução à Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Banco de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '37', '2018'),
+('9', 'Cozinha Árabe', 'Aprender a fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
